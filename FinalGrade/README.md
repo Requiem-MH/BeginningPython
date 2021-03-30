a1fbe8d6d3ebf8eb2aa2a37b024e9420206fce06
Michael Harrop <michael.harrop13@gmail.com>
2021-03-29 22:46:11 -0600

    Added Final Grade Program

---
+++ FinalGrade/README.md
@@ -0,0 +1,12 @@
+This program computes a final grade of 475 possible points using the following:
+Homework: 150 possible points
+Exams: 250 possible points
+Recitation: 75 possible points
+
+The program can be run in the command line using:
+
+'''python gradeCalculator.py```
+
+When run, the program will ask you to seperately enter the three different scores,
+upon entering the 3rd score, it will use the 3 entered scored to compute a final
+percentage and score out of 475.
---
+++ FinalGrade/gradeCalculator.py
@@ -0,0 +1,14 @@
+#Prompt User for Points Earned
+
+homework = eval(input("Please enter how many points earned for homework, out of 150: "))
+exam = eval(input("Please enter how many points earned for exam, out of 250: "))
+recitation = eval(input("Please enter how many points earned for recitation, out of 75: "))
+
+#Calculate Score and Percent
+TOTAL = 475
+score = (homework + exam + recitation)
+percent = ((homework + exam + recitation) / TOTAL) * 100
+
+#Display Final Score and Percentage in class
+print("Total points out of 475: ", score)
+print("Final Percentage: ", int(percent), "%")
